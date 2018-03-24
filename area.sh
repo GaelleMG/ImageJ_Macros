d62ece45198dce1b66a30ba819bfd62379134979
Juan Ortiz <ortizub41@users.noreply.github.com>
2018-03-24 18:50:19 -0400

    Update area.sh

--- area.sh
+++ area.sh
@@ -1,52 +1,66 @@
 #!/bin/bash
 # Juan Ortiz
-#Check if area exists as a directory if not creat it
+
+# Select starting file name
+echo "Enter first few characters of file name:"
+read first_characters
+
+# Check if area exists as a directory if not creat it
 if [ ! -d 'area' ]; then
     mkdir area
 fi
+
 # Move all files ending in area.txt into the directory area
 cp *area.txt area
+
 # Move into the directory area
 cd area
+
 # If the file data.txt already exists delete it
 if [ -f 'data.txt' ]; then
     rm data.txt
 fi
-# Copy selected sorted by filename length into the file data.txt
+
+# Copy selected sorted by size columns into the file data.txt
 for p in `ls | perl -e 'print sort { length($b) <=> length($a) } <>'`;
 do
     ls $p >> data.txt
     cat $p | awk '{print $2}' | sed '1d' >> data.txt
 done
+
 # Go back a directory
 cd ..
+
 # If the current directory exists create a currentdir_area_data.csv file
 if [ -f $(basename "$PWD"_area_data.csv) ]; then
     rm $(basename "$PWD"_area_data.csv)
 fi
+
 # Run python to copy formatted data into currentdir_area.data.csv file
 echo "# Designated path
 path = 'area/data.txt'
 # Check for starting file name
-starting = '387'
+starting = '$first_characters'
 # Store everything in a list
 my_file = []
 # Open the file to read it
 with open(path, 'r') as f:
     my_file = f.read().splitlines()
     for i in my_file:
-        # If the name starts with 387 write it first
+        # If the name in the array starts with (first_characters) write it first
         # otherwise copy its data in a single row
-        # Append a comma to the end of area.txt so that it doesn't append
+        # Need to append a comma at the end of area.txt so that it doesn't append
         # the first number to the end
         if i[-8:] == 'area.txt':
-            i += ','
-        if i[:3] == starting:
+            # Begin data at column letter O
+            i += ',,,,,,,,,,,,'
+        if i[:len(starting)] == starting:
             print '\n{}'.format(i),
         else:
             print '{},'.format(i),
 # Close the file
 f.close()" > area.py
 python area.py >> $(basename "$PWD"_area_data.csv)
-# Delete the directory area and area.py
+
+# Delete the directory area and file area.py
 rm -r area area.py
