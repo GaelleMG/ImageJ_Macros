fc95470a133ad91ad5718818301adb6f1ac7cd5a
Juan Ortiz <ortizub41@gmail.com>
2022-04-12 13:28:55 -0400

    Try number 3

--- coloc.sh
+++ coloc.sh
@@ -46,19 +46,19 @@ try:
         with open('$filename', 'a') as csvfile:
             one, two, three, four, five, six, eight = '', '', '', '', '', '', '', ''
             spamwriter = csv.writer(csvfile)
-            if len(numbers(my_list) > 5):
+            if len(my_list) > 5:
                 one = numbers(my_list[5])
-            if len(numbers(my_list) > 8):
+            if len(numbers(my_list) > 8:
                 two = numbers(my_list[8])
-            if len(numbers(my_list) > 9):
+            if len(my_list) > 9:
                 three = numbers(my_list[9]) 
-            if len(numbers(my_list) > 12):
+            if len(my_list) > 12:
                 four = numbers(my_list[12]) 
-            if len(numbers(my_list) > 13):
+            if len(my_list) > 13:
                 five = numbers(my_list[13])
-            if len(numbers(my_list) > 17):
+            if len(my_list) > 17:
                 six = numbers(my_list[17])
-            if len(numbers(my_list) > 29):
+            if len(my_list) > 29:
                 eight = numbers(my_list[29])
             col = file_name + one + two + three + four + five + six + eight
             spamwriter.writerow(col)
